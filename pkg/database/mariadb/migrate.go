588ae33aacacb500abbaaea7221afc1add8c42c8
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-08-13 15:43:42 +0700

    chore: setup database for migrate

---
+++ pkg/database/mariadb/migrate.go
@@ -0,0 +1,23 @@
+package mariadb
+
+import (
+	"event-management/entity"
+
+	"gorm.io/gorm"
+)
+
+func Migrate(db *gorm.DB) error {
+	err := db.AutoMigrate(
+		&entity.Role{},
+		&entity.User{},
+		&entity.Event{},
+		&entity.Registration{},
+		&entity.Session{},
+		&entity.Payment{},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
