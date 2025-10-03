d3e44d1d6b3ea0b1bd61735201ad9e46ebd1300d
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-03 22:34:10 +0700

    feat: add user model for handling request and response

---
+++ model/user.go
@@ -0,0 +1,34 @@
+package model
+
+type GetUserParam struct {
+	UserID   int     `json:"-"`
+	Email    string  `json:"-"`
+	GoogleID *string `json:"-"`
+}
+
+type UserRegisterParam struct {
+	Name            string `json:"name" binding:"required"`
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required,min=6,max=20"`
+	ConfirmPassword string `json:"confirm_password" binding:"required"`
+}
+
+type UserLoginParam struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserLoginResponse struct {
+	Token string `json:"token"`
+}
+
+type GoogleUserInfo struct {
+	ID      string `json:"id"`
+	Email   string `json:"email"`
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
+}
+
+type OAuthLoginResponse struct {
+	Token string `json:"token"`
+}
