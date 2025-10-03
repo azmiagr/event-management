70df2706188c2afa7f11f1e530845549a67957d1
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-03 22:31:28 +0700

    feat: add GoogleID and Picture attributes to User

--- entity/user.go
+++ entity/user.go
@@ -9,9 +9,11 @@ import (
 type User struct {
 	UserID    uuid.UUID `json:"user_id" gorm:"type:varchar(36);primaryKey"`
 	RoleID    int       `json:"role_id"`
+	GoogleID  *string   `json:"google_id" gorm:"uniqueIndex"`
+	Picture   *string   `json:"picture"`
 	Name      string    `json:"name" gorm:"type:varchar(100)"`
 	Email     string    `json:"email" gorm:"type:varchar(100)"`
-	Password  string    `json:"password" gorm:"type:varchar(100)"`
+	Password  *string   `json:"password" gorm:"type:varchar(100)"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 
