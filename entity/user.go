42d942fab99c60831a94dcc019e6b9419f39cbfc
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-04 14:28:28 +0700

    feat: add relation to otp entity

--- entity/user.go
+++ entity/user.go
@@ -7,16 +7,18 @@ import (
 )
 
 type User struct {
-	UserID    uuid.UUID `json:"user_id" gorm:"type:varchar(36);primaryKey"`
-	RoleID    int       `json:"role_id"`
-	GoogleID  *string   `json:"google_id" gorm:"uniqueIndex"`
-	Picture   *string   `json:"picture"`
-	Name      string    `json:"name" gorm:"type:varchar(100)"`
-	Email     string    `json:"email" gorm:"type:varchar(100)"`
-	Password  *string   `json:"password" gorm:"type:varchar(100)"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	UserID        uuid.UUID `json:"user_id" gorm:"type:varchar(36);primaryKey"`
+	RoleID        int       `json:"role_id"`
+	GoogleID      *string   `json:"google_id" gorm:"uniqueIndex"`
+	Picture       *string   `json:"picture"`
+	Name          string    `json:"name" gorm:"type:varchar(100)"`
+	Email         string    `json:"email" gorm:"type:varchar(100)"`
+	Password      *string   `json:"password" gorm:"type:varchar(100)"`
+	StatusAccount string    `json:"-" gorm:"type:enum('inactive', 'active');default:'inactive'"`
+	CreatedAt     time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 
 	Events        []Event        `json:"events" gorm:"foreignKey:UserID"`
 	Registrations []Registration `json:"registrations" gorm:"foreignKey:UserID"`
+	Otp           []Otp          `json:"otp" gorm:"foreignKey:UserID"`
 }
