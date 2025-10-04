fdd43e28f8db03712ce3fba11f7be85a248488bc
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-04 14:23:40 +0700

    feat: add otp dependencies in service layer

--- internal/service/service.go
+++ internal/service/service.go
@@ -10,11 +10,13 @@ import (
 type Service struct {
 	UserService  IUserService
 	OAuthService IOAuthService
+	OtpService   IOtpService
 }
 
 func NewService(repo *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, oauth *config.OAuthConfig) *Service {
 	return &Service{
-		UserService:  NewUserService(repo.UserRepository, bcrypt, jwtAuth),
+		UserService:  NewUserService(repo.UserRepository, repo.OtpRepository, bcrypt, jwtAuth),
 		OAuthService: NewOAuthService(repo.UserRepository, bcrypt, jwtAuth, oauth),
+		OtpService:   NewOtpService(repo.OtpRepository, repo.UserRepository),
 	}
 }
