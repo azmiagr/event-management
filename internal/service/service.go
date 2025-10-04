130902873825d0b456f5a6018fb1825ecf63e301
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-04 13:12:41 +0700

    feat: add user and oauth service to the dependencies

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,4 +1,20 @@
 package service
 
+import (
+	"event-management/internal/repository"
+	"event-management/pkg/bcrypt"
+	"event-management/pkg/config"
+	"event-management/pkg/jwt"
+)
+
 type Service struct {
+	UserService  IUserService
+	OAuthService IOAuthService
+}
+
+func NewService(repo *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, oauth *config.OAuthConfig) *Service {
+	return &Service{
+		UserService:  NewUserService(repo.UserRepository, bcrypt, jwtAuth),
+		OAuthService: NewOAuthService(repo.UserRepository, bcrypt, jwtAuth, oauth),
+	}
 }
