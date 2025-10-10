607921d9911e40d09a1f9aff7cd0e05865a3bcb9
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 17:31:28 +0700

    feat: add OnlyAdmin middleware to restrict access to admin users based on role ID

---
+++ pkg/middleware/authorization.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"errors"
+	"event-management/pkg/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (m *middleware) OnlyAdmin(c *gin.Context) {
+	user, err := m.jwtAuth.GetLoginUser(c)
+	if err != nil {
+		response.Error(c, http.StatusForbidden, "failed to get login user", err)
+		c.Abort()
+		return
+	}
+
+	if user.RoleID != 1 {
+		response.Error(c, http.StatusForbidden, "this endpoint cannot be access", errors.New("user dont have access"))
+		c.Abort()
+		return
+	}
+	c.Next()
+}
