db737172ef79a63ba86219760c2b0ba013e9a3d0
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 17:29:32 +0700

    feat: extend Service struct to include RegistrationService, EventService, and PaymentService for enhanced event management functionality

--- internal/service/service.go
+++ internal/service/service.go
@@ -5,18 +5,29 @@ import (
 	"event-management/pkg/bcrypt"
 	"event-management/pkg/config"
 	"event-management/pkg/jwt"
+
+	"github.com/midtrans/midtrans-go/coreapi"
+	"github.com/midtrans/midtrans-go/snap"
 )
 
 type Service struct {
-	UserService  IUserService
-	OAuthService IOAuthService
-	OtpService   IOtpService
+	UserService         IUserService
+	OAuthService        IOAuthService
+	OtpService          IOtpService
+	RegistrationService IRegistrationService
+	EventService        IEventService
+	PaymentService      IPaymentService
 }
 
-func NewService(repo *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, oauth *config.OAuthConfig) *Service {
+func NewService(repo *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, oauth *config.OAuthConfig, snapClient snap.Client, coreAPIClient coreapi.Client) *Service {
+	paymentService := NewPaymentService(repo.PaymentRepository, snapClient, coreAPIClient)
+
 	return &Service{
-		UserService:  NewUserService(repo.UserRepository, repo.OtpRepository, bcrypt, jwtAuth),
-		OAuthService: NewOAuthService(repo.UserRepository, bcrypt, jwtAuth, oauth),
-		OtpService:   NewOtpService(repo.OtpRepository, repo.UserRepository),
+		UserService:         NewUserService(repo.UserRepository, repo.OtpRepository, bcrypt, jwtAuth),
+		OAuthService:        NewOAuthService(repo.UserRepository, bcrypt, jwtAuth, oauth),
+		OtpService:          NewOtpService(repo.OtpRepository, repo.UserRepository),
+		RegistrationService: NewRegistrationService(repo.RegistrationRepository, repo.EventRepository, paymentService),
+		EventService:        NewEventService(repo.EventRepository),
+		PaymentService:      paymentService,
 	}
 }
