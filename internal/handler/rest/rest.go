49ca35b5edc9c82b3990bd07e714164639625f3a
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 17:27:25 +0700

    feat: enhance REST handler with middleware integration and new event registration endpoints

--- internal/handler/rest/rest.go
+++ internal/handler/rest/rest.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"event-management/internal/service"
+	"event-management/pkg/middleware"
 	"fmt"
 	"os"
 
@@ -9,14 +10,16 @@ import (
 )
 
 type Rest struct {
-	router  *gin.Engine
-	service *service.Service
+	router     *gin.Engine
+	service    *service.Service
+	middleware middleware.Interface
 }
 
-func NewRest(service *service.Service) *Rest {
+func NewRest(service *service.Service, middleware middleware.Interface) *Rest {
 	return &Rest{
-		router:  gin.Default(),
-		service: service,
+		router:     gin.Default(),
+		service:    service,
+		middleware: middleware,
 	}
 }
 
@@ -25,12 +28,24 @@ func (r *Rest) MountEndpoint() {
 
 	auth := router.Group("/auth")
 	auth.POST("/register", r.Register)
+	auth.PATCH("/register", r.VerifyUser)
+	auth.PATCH("/register/resend", r.ResendOtp)
 	auth.POST("/login", r.Login)
 
 	google := auth.Group("/google")
 	google.GET("/login", r.GoogleLogin)
 	google.GET("/callback", r.GoogleCallback)
 
+	event := router.Group("/events")
+	event.Use(r.middleware.AuthenticateUser)
+	event.POST("/add-event", r.CreateEvent)
+
+	registration := router.Group("/registrations")
+	registration.Use(r.middleware.AuthenticateUser)
+	registration.POST("/event", r.RegisterEvent)
+
+	notification := router.Group("/notification")
+	notification.POST("", r.HandleNotification)
 }
 
 func (r *Rest) Run() {
