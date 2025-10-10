0f3af38571c079e82bd888af95747bf3e6e7d004
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 17:31:59 +0700

    feat: add timeout middleware to enforce request time limits and handle timeout responses

---
+++ pkg/middleware/timeout.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"errors"
+	"event-management/pkg/response"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-contrib/timeout"
+	"github.com/gin-gonic/gin"
+)
+
+func (m *middleware) Timeout() gin.HandlerFunc {
+	timeLimit, _ := strconv.Atoi(os.Getenv("TIME_OUT_LIMIT"))
+
+	return timeout.New(
+		timeout.WithTimeout(time.Duration(timeLimit)*time.Second),
+		timeout.WithResponse(timeoutResponse),
+	)
+}
+
+func timeoutResponse(c *gin.Context) {
+	response.Error(c, http.StatusRequestTimeout, "the request take to much time", errors.New(""))
+}
