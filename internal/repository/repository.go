2089970adea42c7bcd0ef433dbbb8f99e37e5bed
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 17:28:35 +0700

    feat: add PaymentRepository, RegistrationRepository, and EventRepository to the Repository struct

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,13 +3,19 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-	UserRepository IUserRepository
-	OtpRepository  IOtpRepository
+	UserRepository         IUserRepository
+	OtpRepository          IOtpRepository
+	PaymentRepository      IPaymentRepository
+	RegistrationRepository IRegistrationRepository
+	EventRepository        IEventRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		UserRepository: NewUserRepository(db),
-		OtpRepository:  NewOtpRepository(db),
+		UserRepository:         NewUserRepository(db),
+		OtpRepository:          NewOtpRepository(db),
+		PaymentRepository:      NewPaymentRepository(db),
+		RegistrationRepository: NewRegistrationRepository(db),
+		EventRepository:        NewEventRepository(db),
 	}
 }
