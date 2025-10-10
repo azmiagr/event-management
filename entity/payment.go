3ef4e54ff69878f3f43ab94c79006597680b696f
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 17:26:23 +0700

    feat: update Payment entity structure with new fields and types

--- entity/payment.go
+++ entity/payment.go
@@ -7,10 +7,13 @@ import (
 )
 
 type Payment struct {
-	PaymentID      uuid.UUID `json:"payment_id" gorm:"type:varchar(36);primaryKey"`
+	OrderID        string    `json:"order_id" gorm:"type:varchar(255);primaryKey"`
 	RegistrationID uuid.UUID `json:"registration_id"`
 	Amount         float64   `json:"amount" gorm:"type:float(8,2)"`
-	Method         string    `json:"method" gorm:"type:varchar(20)"`
-	Status         string    `json:"status" gorm:"type:enum('pending','paid','failed')"`
+	Status         string    `json:"status" gorm:"type:varchar(30)"`
+	SnapURL        string    `json:"snap_url" gorm:"type:varchar(255)"`
+	PaymentType    string    `json:"payment_type" gorm:"type:varchar(50)"`
 	PaidAt         time.Time `json:"paid_at" gorm:"type:datetime;default:null"`
+	CreatedAt      time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt      time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
